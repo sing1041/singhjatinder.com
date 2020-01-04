7cf4d747e16d5b3fca91c8280f5e189b5999a6ed
Jatinder Singh <50679467+sing1041@users.noreply.github.com>
2020-01-03 23:12:59 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
-// jQuery(document).on('mouseover','.work_content_scroller',function(){
-// 	var hash = $(this);
-// 	hash.animate({ scrollTop: hash.find('img').height()-100 }, 8000);
-// });
-
+ jQuery(document).on('click','.work_content_scroller,.scrollImg',function(e){
+  e.preventDefault();
+  var self = jQuery(this); 
+  var height = 10000;
+  if(self.hasClass('scroller_mobile_image')) {
+     height = 18000;
+  }
+  if(self.scrollTop() > 0 ) {
+    self.animate({ scrollTop: 0 }, height);
+  }else{
+    self.animate({ scrollTop: self.find('img').height() }, height);
+  }
+ 	
+ });
 
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
@@ -15,11 +24,9 @@ hamburger.addEventListener("click", () => {
 	});
 });
 
-
-
 $(document).on('click', '.navClick', function (event) {
     event.preventDefault();
-    $(document).find(".nav-links").removeClass('open');
+
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 1000);
@@ -53,68 +60,3 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-
-
-
-
-// intro typwriter effect
-
-var TxtType = function(el, toRotate, period) {
-        this.toRotate = toRotate;
-        this.el = el;
-        this.loopNum = 0;
-        this.period = parseInt(period, 10) || 500;
-        this.txt = '';
-        this.tick();
-        this.isDeleting = false;
-    };
-
-    TxtType.prototype.tick = function() {
-        var i = this.loopNum % this.toRotate.length;
-        var fullTxt = this.toRotate[i];
-
-        if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-        } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-        }
-
-        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-        var that = this;
-        var delta = 200 - Math.random() * 100;
-
-        if (this.isDeleting) { delta /= 2; }
-
-        if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-        } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
-        }
-
-        setTimeout(function() {
-        that.tick();
-        }, delta);
-    };
-
-    window.onload = function() {
-        var elements = document.getElementsByClassName('typewrite');
-        for (var i=0; i<elements.length; i++) {
-            var toRotate = elements[i].getAttribute('data-type');
-            var period = elements[i].getAttribute('data-period');
-            if (toRotate) {
-              new TxtType(elements[i], JSON.parse(toRotate), period);
-            }
-        }
-        // INJECT CSS
-        var css = document.createElement("style");
-        css.type = "text/css";
-        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-        document.body.appendChild(css);
-    };
-
-      
-
